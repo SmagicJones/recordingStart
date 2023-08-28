d55274edb4e3d7704253407edb0d7f3bb700fe14
Smagic Jones <>
2023-08-28 15:08:29 +0100

    record and play fixed

--- App.js
+++ App.js
@@ -1,3 +1,112 @@
+// import * as React from 'react';
+// import { Text, View, StyleSheet, Button } from 'react-native';
+// import { Audio } from 'expo-av';
+// import { setStatusBarBackgroundColor } from 'expo-status-bar';
+
+// export default function App() {
+//   const [recordings, setRecordings] = React.useState();
+//   const [recording, setRecording] = React.useState();
+//   const [loadedSound, setLoadedSound] = React.useState();
+
+
+
+//   async function startRecording() {
+//     try {
+//       console.log('Requesting permissions..');
+//       await Audio.requestPermissionsAsync();
+//       await Audio.setAudioModeAsync({
+//         allowsRecordingIOS: true,
+//         playsInSilentModeIOS: true,
+//       });
+
+//       console.log('Starting recording..');
+//       const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY
+//       );
+//       setRecording(recording);
+//       console.log('Recording started');
+//     } catch (err) {
+//       console.error('Failed to start recording', err);
+//     }
+//   }
+
+//   let recordingLocation = undefined;
+
+//   async function stopRecording() {
+//     console.log('Stopping recording..');
+//     setRecording(undefined);
+//     await recording.stopAndUnloadAsync();
+//     await Audio.setAudioModeAsync(
+//       {
+//         allowsRecordingIOS: false,
+//       }
+//     );
+//     const URI = recording.getURI();
+
+//     };
+
+//     setRecordings(updatedRecordings)
+//     // console.log('Recording stopped and stored at', recordingLocation);
+//   }
+
+//   function getDurationFormatted(millis){
+//     const minutes = millis / 1000 / 60;
+//     const minutesDisplay = Math.floor(minutes);
+//     const seconds = Math.round((minutes - minutesDisplay) * 60);
+//     const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
+
+//     return `${minutesDisplay}:${secondsDisplay}`
+//   }
+
+//   function getRecordingLines(){
+//     return recordings.map((recordingLine, index) => {
+//       return (
+//         <View key={index} style={styles.row}>
+//           <Text style={styles.fill}>Recording {index + 1} - {recordingLine.duration}</Text>
+//           <Button style={styles.button} onPress={()=> recordingLine.sound.replayAsync()} title="Play"></Button>
+//           <Button style={styles.button} onPress={()=> Sharing.shareAsync(recordingLine.file)} title="share"></Button>
+
+//         </View>
+//       )
+//     })
+//   }
+
+
+
+
+//   async function playSound(){
+//     console.log('loading sound');
+//     const {sound} = await Audio.Sound.createAsync(require('./assets/crash.mp3'))
+//     setLoadedSound(sound)
+//     console.log('playing sound');
+//     await loadedSound.playAsync()
+//   }
+
+ 
+
+//   return (
+//     <View style={styles.container}>
+//       <Button
+//         title={recording ? 'Stop Recording' : 'Start Recording'}
+//         onPress={recording ? stopRecording : startRecording}
+//       />
+//       <Button
+//       title='play'
+//       onPress={playSound}
+//       ></Button>
+//       {getRecordingLines()}
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//       },
+// }); 
+
 // import { StatusBar } from 'expo-status-bar';
 // import { useState } from 'react';
 // import { Button, StyleSheet, Text, View } from 'react-native';
@@ -103,12 +212,14 @@
 // });
 
 import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { Audio } from 'expo-av';
 import { setStatusBarBackgroundColor } from 'expo-status-bar';
 
 export default function App() {
   const [recording, setRecording] = React.useState();
+  const [recordings, setRecordings] = React.useState([]);
   const [loadedSound, setLoadedSound] = React.useState()
 
   async function startRecording() {
@@ -141,6 +252,34 @@ export default function App() {
     );
     const uri = recording.getURI();
     console.log('Recording stopped and stored at', uri);
+    let updatedRecordings = [...recordings]
+    const {sound, status} = await recording.createNewLoadedSoundAsync();
+    updatedRecordings.push({
+      sound: sound,
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI()
+    });
+    setRecordings(updatedRecordings);
+  }
+
+  function getDurationFormatted(millis) {
+    const minutes = millis / 1000 / 60;
+    const minutesDisplay = Math.floor(minutes);
+    const seconds = Math.round((minutes - minutesDisplay) * 60);
+    const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
+    return `${minutesDisplay}:${secondsDisplay}`;
+  }
+
+  function getRecordingLines() {
+    return recordings.map((recordingLine, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          <Text style={styles.fill}>Recording {index + 1} - {recordingLine.duration}</Text>
+          <Button style={styles.button} onPress={() => recordingLine.sound.replayAsync()} title="Play"></Button>
+          {/* <Button style={styles.button} onPress={() => Sharing.shareAsync(recordingLine.file)} title="Share"></Button> */}
+        </View>
+      );
+    });
   }
 
   async function playSound(){
@@ -162,15 +301,28 @@ export default function App() {
       title='play'
       onPress={playSound}
       ></Button>
+      {getRecordingLines()}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-}); 
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fill: {
+    flex: 1,
+    margin: 16
+  },
+  button: {
+    margin: 16
+  }
+});
