151a7482cd9aa318ee831a3ac8e508f2d4788349
Smagic Jones <>
2023-08-28 15:14:07 +0100

    tidying up

--- App.js
+++ App.js
@@ -1,216 +1,3 @@
-// import * as React from 'react';
-// import { Text, View, StyleSheet, Button } from 'react-native';
-// import { Audio } from 'expo-av';
-// import { setStatusBarBackgroundColor } from 'expo-status-bar';
-
-// export default function App() {
-//   const [recordings, setRecordings] = React.useState();
-//   const [recording, setRecording] = React.useState();
-//   const [loadedSound, setLoadedSound] = React.useState();
-
-
-
-//   async function startRecording() {
-//     try {
-//       console.log('Requesting permissions..');
-//       await Audio.requestPermissionsAsync();
-//       await Audio.setAudioModeAsync({
-//         allowsRecordingIOS: true,
-//         playsInSilentModeIOS: true,
-//       });
-
-//       console.log('Starting recording..');
-//       const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY
-//       );
-//       setRecording(recording);
-//       console.log('Recording started');
-//     } catch (err) {
-//       console.error('Failed to start recording', err);
-//     }
-//   }
-
-//   let recordingLocation = undefined;
-
-//   async function stopRecording() {
-//     console.log('Stopping recording..');
-//     setRecording(undefined);
-//     await recording.stopAndUnloadAsync();
-//     await Audio.setAudioModeAsync(
-//       {
-//         allowsRecordingIOS: false,
-//       }
-//     );
-//     const URI = recording.getURI();
-
-//     };
-
-//     setRecordings(updatedRecordings)
-//     // console.log('Recording stopped and stored at', recordingLocation);
-//   }
-
-//   function getDurationFormatted(millis){
-//     const minutes = millis / 1000 / 60;
-//     const minutesDisplay = Math.floor(minutes);
-//     const seconds = Math.round((minutes - minutesDisplay) * 60);
-//     const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
-
-//     return `${minutesDisplay}:${secondsDisplay}`
-//   }
-
-//   function getRecordingLines(){
-//     return recordings.map((recordingLine, index) => {
-//       return (
-//         <View key={index} style={styles.row}>
-//           <Text style={styles.fill}>Recording {index + 1} - {recordingLine.duration}</Text>
-//           <Button style={styles.button} onPress={()=> recordingLine.sound.replayAsync()} title="Play"></Button>
-//           <Button style={styles.button} onPress={()=> Sharing.shareAsync(recordingLine.file)} title="share"></Button>
-
-//         </View>
-//       )
-//     })
-//   }
-
-
-
-
-//   async function playSound(){
-//     console.log('loading sound');
-//     const {sound} = await Audio.Sound.createAsync(require('./assets/crash.mp3'))
-//     setLoadedSound(sound)
-//     console.log('playing sound');
-//     await loadedSound.playAsync()
-//   }
-
- 
-
-//   return (
-//     <View style={styles.container}>
-//       <Button
-//         title={recording ? 'Stop Recording' : 'Start Recording'}
-//         onPress={recording ? stopRecording : startRecording}
-//       />
-//       <Button
-//       title='play'
-//       onPress={playSound}
-//       ></Button>
-//       {getRecordingLines()}
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//         flex: 1,
-//         backgroundColor: '#fff',
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//       },
-// }); 
-
-// import { StatusBar } from 'expo-status-bar';
-// import { useState } from 'react';
-// import { Button, StyleSheet, Text, View } from 'react-native';
-// import { Audio } from 'expo-av';
-
-// export default function App() {
-//   const [recording, setRecording] = useState();
-//   const [recordings, setRecordings] = useState([]);
-//   const [message, setMessage] = useState("");
-
-
-//   async function startRecording(){
-//     try {
-//       const permission = await Audio.requestPermissionsAsync();
-
-//       if(permission.status === "granted"){
-//         await Audio.setAudioModeAsync({
-//           allowRecordingIOS: true,
-//           playInSilentModeIOS: true
-//         });
-//         const {recording} = await Audio.Recording.createAsync(
-//           Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
-//         );
-//         setRecording(recording);
-//       } else {
-//         setMessage("Please allow access to your microphone")
-//       }
-//     } catch (err){
-//       console.error("failed to start recording", err)
-//     }
-
-//   }
-
-//    async function stopRecording(){
-//     setRecording(undefined);
-//     await recording.stopAndUnloadAsync();
-
-//     let updatedRecordings = [...recordings];
-//     const {sound, status} = await recording.createNewLoadedSoundAsync();
-//     updatedRecordings.push({
-//       sound: sound,
-//       duration: getDurationFormatted(status.durationMillis),
-//       file: recording.getURI()
-//     });
-
-//     setRecordings(updatedRecordings);
-//   }
-
-//   function getDurationFormatted(millis){
-//     const minutes = millis / 1000 * 60;
-//     const minutesDisplay = Math.floor(minutes);
-//     const seconds = Math.round(minutes - minutesDisplay) * 60;
-//     const secondsDisplay = seconds < 10 ? `0${seconds}` :  seconds;
-//     return `${minitesDisplay}:${secondsDisplay}`;
-//   }
-
-//   function getRecordingLines(){
-//     return recordings.map((recordingLine, i)=>{
-//       return (
-//         <View key={i} style={styles.row}>
-//         <Text style={styles.fill}>Recording {i +1} - {recordingLine.duration}</Text>
-//         <Button style={styles.button} onPress={()=> recordingLine.sound.replayAsync()} title='play '></Button>
-
-//         </View>
-//       )
-//     })
-//   }
-
-
-
-
-//   return (
-//     <View style={styles.container}>
-//       <Text>{message}</Text>
-//       <Button
-//         title={recording ? "Stop Recording" : "Start Recording"}
-//         onPress={recording ? stopRecording : startRecording}
-
-//       />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   row: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   fill: {
-//     flex: 1,
-//     margin: 16
-//   },
-//   button: {
-//     margin: 16
-//   }
-// });
-
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, StyleSheet, Button } from 'react-native';
@@ -220,7 +7,6 @@ import { setStatusBarBackgroundColor } from 'expo-status-bar';
 export default function App() {
   const [recording, setRecording] = React.useState();
   const [recordings, setRecordings] = React.useState([]);
-  const [loadedSound, setLoadedSound] = React.useState()
 
   async function startRecording() {
     try {
@@ -282,14 +68,13 @@ export default function App() {
     });
   }
 
-  async function playSound(){
-    console.log('loading sound');
-    const {sound} = await Audio.Sound.createAsync(require('./assets/crash.mp3'))
-    setLoadedSound(sound)
-    console.log('playing sound');
-    await loadedSound.playAsync()
-
-  }
+//   async function playSound(){
+//     console.log('loading sound');
+//     const {sound} = await Audio.Sound.createAsync(require('./assets/crash.mp3'))
+//     setLoadedSound(sound)
+//     console.log('playing sound');
+//     await loadedSound.playAsync()
+// }
 
   return (
     <View style={styles.container}>
@@ -297,10 +82,6 @@ export default function App() {
         title={recording ? 'Stop Recording' : 'Start Recording'}
         onPress={recording ? stopRecording : startRecording}
       />
-      <Button
-      title='play'
-      onPress={playSound}
-      ></Button>
       {getRecordingLines()}
     </View>
   );
